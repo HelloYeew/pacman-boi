81d672dee4f35eb6f6db928449be24e329020be5
pontakornth <pontakorn61@gmail.com>
2021-03-18 09:47:34 +0700

    refactor: Make use of command pattern
    Resolve #2

--- main.py
+++ main.py
@@ -55,6 +55,19 @@ class PacmanGame(GameApp):
 
         self.elements.append(self.pacman1)
         self.elements.append(self.pacman2)
+        self.command_map = {
+            # Pacman 1
+            'W': self.get_pacman_next_direction_function(self.pacman1, DIR_UP),
+            'A': self.get_pacman_next_direction_function(self.pacman1, DIR_LEFT),
+            'S': self.get_pacman_next_direction_function(self.pacman1, DIR_DOWN),
+            'D': self.get_pacman_next_direction_function(self.pacman1, DIR_RIGHT),
+
+            # Pacman 2
+            'I': self.get_pacman_next_direction_function(self.pacman2, DIR_UP),
+            'J': self.get_pacman_next_direction_function(self.pacman2, DIR_LEFT),
+            'K': self.get_pacman_next_direction_function(self.pacman2, DIR_DOWN),
+            'L': self.get_pacman_next_direction_function(self.pacman2, DIR_RIGHT),
+        }
 
     def pre_update(self):
         pass
@@ -62,24 +75,17 @@ class PacmanGame(GameApp):
     def post_update(self):
         pass
 
-    def on_key_pressed(self, event):
-        if event.char.upper() == 'A':
-            self.pacman1.set_next_direction(DIR_LEFT)
-        elif event.char.upper() == 'W':
-            self.pacman1.set_next_direction(DIR_UP)
-        elif event.char.upper() == 'S':
-            self.pacman1.set_next_direction(DIR_DOWN)
-        elif event.char.upper() == 'D':
-            self.pacman1.set_next_direction(DIR_RIGHT)
+    def get_pacman_next_direction_function(self, pacman, next_direction):
+        def inner():
+            pacman.set_next_direction(next_direction)
+        return inner
 
-        if event.char.upper() == 'J':
-            self.pacman2.set_next_direction(DIR_LEFT)
-        elif event.char.upper() == 'I':
-            self.pacman2.set_next_direction(DIR_UP)
-        elif event.char.upper() == 'K':
-            self.pacman2.set_next_direction(DIR_DOWN)
-        elif event.char.upper() == 'L':
-            self.pacman2.set_next_direction(DIR_RIGHT)
+    def on_key_pressed(self, event):
+        key = event.char.upper()
+        command = self.command_map.get(key)
+        if command:
+            command()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
